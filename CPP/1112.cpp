6c46f5104f9db44d574325b46f02d3ed0ea48d1d
slutsk <54749775+slutsk@users.noreply.github.com>
2023-01-14 16:25:04 +0300

    Create 1112.cpp

---
+++ CPP/1112.cpp
@@ -0,0 +1,15 @@
+// 1112 Сумма цифр числа
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c;
+    cin >> n;
+    a = n / 100;
+    b = n / 10 % 10;
+    c = n % 10;
+    cout << a + b + c;
+}
+
+
