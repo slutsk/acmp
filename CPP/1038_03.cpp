2071801fda225f8505d1ed243cc2c06a63e94bf7
slutsk <54749775+slutsk@users.noreply.github.com>
2023-01-14 16:58:03 +0300

    Create 1038_03.cpp

---
+++ CPP/1038_03.cpp
@@ -0,0 +1,16 @@
+// 1038 Перевязь 
+/*
+* Второй СПОСОБ - округление вверх
+  
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << (n + 9) / 10;
+}
+
+
